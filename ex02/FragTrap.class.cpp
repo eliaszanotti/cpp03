4825ad42fb6b175575a555b612fce5c49d17918c
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-05-23 14:22:44 +0200

    fix merge

--- ex02/FragTrap.class.cpp
+++ ex02/FragTrap.class.cpp
@@ -16,34 +16,36 @@
 // Constructors
 FragTrap::FragTrap()
 {
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
 	std::cout << "\e[32m[FragTrap] " << this->_name << " created.\e[0m" \
 		<< std::endl;
 }
 
 FragTrap::FragTrap(std::string name): ClapTrap(name)
 {
-	std::cout << "\e[32m[FragTrap] " << this->_name << " created.\e[0m" \
-		<< std::endl;
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+	std::cout << "\e[32m[FragTrap] " << this->_name << " created.\e[0m" << std::endl;
 }
 
 FragTrap::FragTrap(FragTrap const &copy): ClapTrap(copy)
 {
-	std::cout << "\e[32m[FragTrap] " << copy._name << " copied.\e[0m" \
-		<< std::endl;
+	std::cout << "\e[32m[FragTrap] " << copy._name << " copied.\e[0m" << std::endl;
 }
 
 FragTrap::~FragTrap()
 {
-	std::cout << "\e[31m[FragTrap] " << this->_name << " destroyed.\e[0m" \
-		<< std::endl;
+	std::cout << "\e[31m[FragTrap] " << this->_name << " destroyed.\e[0m" << std::endl;
 }
 
 // Operators
 FragTrap	&FragTrap::operator=(FragTrap const &copy)
 {
 	ClapTrap::operator=(copy);
-	std::cout << "\e[32m[FragTrap] " << this->_name << " created by copy (" \
-		<< copy.getName() << ")\e[0m" << std::endl;
+	std::cout << "\e[32m[FragTrap] " << this->_name << " created by copy (" << copy.getName() << ")\e[0m" << std::endl;
 	return (*this);
 }
 
